432b7f2df20b2db1d25f9ec7064bb57519302083
alexis <alexis_jalbert@hotmail.com>
2016-08-06 13:37:11 -0400

    added some structure and styling, began first edition

--- page_creator.py
+++ page_creator.py
@@ -1,15 +1,30 @@
+# coding=utf-8
 class NavBar:
-    def __init__(self):
+    def __init__(self, date, volume):
         self.page_list = []
+        self.date = date
+        self.volume = volume
 
     def add_page_nav(self, number, page_name):
         self.page_list.append('<li><a href="#section{}">{}</a></li>'.format(number, page_name))
 
     def render(self):
-        markup = '''<nav class="navbar navbar-inverse" data-spy="affix" data-offset-top="197">
+        markup = self._add_jumbotron()
+        markup += '''<nav class="navbar navbar-inverse" data-spy="affix" data-offset-top="250">
         <ul class="nav navbar-nav">'''
         markup += ''.join(li for li in self.page_list)
         return markup + '</ul></nav>'
+
+    def _add_jumbotron(self):
+        return '''
+        <div class="container">
+            <div class="jumbotron">
+                <h1>Le Régisseur</h1>
+                <h4>Le journal étudiant du Regroupement des Étudiants de Génie Informatique et Électrique</h4>
+                <h5>Volume {vol} - Édition {edition}</h5>
+            </div>
+        </div>
+        '''.format(vol=self.volume, edition=self.date)
 
 
 class Page:
@@ -25,11 +40,11 @@ class PageCreator:
         self.output_file = 'regisseur{}.html'.format(self.volume)
         self.title_div = ''
         self.body = ''
-        self.navbar = NavBar()
+        self.navbar = NavBar(self.date, self.volume)
         self.style_appends = ''
         self.page_list = []
-        self.fuckyou = None
-
+        self.host = "https://cdn.rawgit.com/AlexisJa/regisseur/master/"
+        self.commit_id = ''
 
     def render(self):
         for i in range(len(self.page_list)):
@@ -40,10 +55,12 @@ class PageCreator:
     def _add_content(self, number, page):
         self.navbar.add_page_nav(number, page.title)
         with open(page.file, 'r') as page_file:
-            self.body += self._wrap_page(number, page_file.read())
+            self.body += self._wrap_page(number, page.title, page_file.read())
 
-    def _wrap_page(self, number, file_content):
-        return '''<div id="section{}" class="container-fluid PageDiv">{}</div>'''.format(number, file_content)
+    def _wrap_page(self, number, title, file_content):
+        return '''<div id="section{number}" class="container-fluid PageDiv">
+        <h2>{title}</h2>
+        {content}</div>'''.format(number=number, content=file_content, title=title)
 
     def _wrap_file(self):
         with open('wrapping.html', 'r') as template:
@@ -53,7 +70,6 @@ class PageCreator:
         with open('regisseur.js', 'r') as js_file:
             script = js_file.read()
         style += self.style_appends
-
 
         return template_string.format(
             style=style,
@@ -65,6 +81,18 @@ class PageCreator:
 
 if __name__ == '__main__':
     p = PageCreator()
-    p.page_list = [Page('page1.html', 'Test')]
+    p.page_list = [
+        Page('r1/new_regisseur.html', 'Bonne rentrée!'),
+        # Page('r0/p2.html', 'Page 2'),
+        # Page('r0/p2.html', 'Page 3'),
+        # Page('r0/p1.html', 'Page 4'),
+        # Page('r0/p1.html', 'Page 5'),
+        # Page('r0/p1.html', 'Page 1'),
+        # Page('r0/p2.html', 'Page 2'),
+        # Page('r0/p2.html', 'Page 3'),
+        # Page('r0/p1.html', 'Page 4'),
+        # Page('r0/p1.html', 'Page 5'),
+
+    ]
     p.render()
     print('ready')
