b49c0cc557f65b6adad2ca8335f4fb0e0da76fa7
Alexis Jalbert <alexis_jalbert@hotmail.com>
2016-09-07 13:31:44 -0400

    added initiation pics

--- regisseur.js
+++ regisseur.js
@@ -3,14 +3,21 @@ var ng_reg = angular.module("regisseur", []);
 ng_reg.controller('CarouselCtrl', function($scope){
     $scope.photos = [];
     $scope.clicked = function(){
-        console.log($scope.hello);
+
     };
-    $scope.init = function(location, photo_array){
-        console.log(photo_array);
+    $scope.init = function(location, caption_dict){
+        console.log(caption_dict);
 
         $scope.photos = [];
-        for (i = 0; i<photo_array.length; i++){
-            $scope.photos.push({src: location + photo_array[i][0], caption: photo_array[i][1]});
+        for (i = 0; i<67; i++){
+            var caption = '';
+            for (j = 0; j<caption_dict.length; j++){
+                if (caption_dict[j].id === i){
+                    caption = caption_dict[j].caption;
+                }
+            }
+            $scope.photos.push({src: location + i+'.jpg', caption: caption});
+            
             // $scope.photos[i].src = location + photo_array[i][0];
             // $scope.photos[i].caption = photo_array[i][1];
         }
