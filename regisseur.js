fd1ddea6774080fb30847393ffcb528a59764f3b
Alexis Jalbert <alexis_jalbert@hotmail.com>
2016-09-07 14:01:01 -0400

    FIRST DONE

--- regisseur.js
+++ regisseur.js
@@ -6,22 +6,19 @@ ng_reg.controller('CarouselCtrl', function($scope){
 
     };
     $scope.init = function(location, caption_dict){
-        console.log(caption_dict);
-
         $scope.photos = [];
-        for (i = 0; i<67; i++){
+        for (i = 1; i<67; i++){
             var caption = '';
-            for (j = 0; j<caption_dict.length; j++){
-                if (caption_dict[j].id === i){
-                    caption = caption_dict[j].caption;
-                }
-            }
+            // for (j = 0; j<caption_dict.length; j++){
+            //     if (caption_dict[j].id === i){
+            //         caption = caption_dict[j].caption;
+            //     }
+            // }
             $scope.photos.push({src: location + i+'.jpg', caption: caption});
             
             // $scope.photos[i].src = location + photo_array[i][0];
             // $scope.photos[i].caption = photo_array[i][1];
         }
-        console.log($scope.photos);
 
     }
 });
