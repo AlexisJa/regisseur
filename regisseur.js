9affac7dd8237c1f470b68484ea1116165b556b9
alexis <alexis_jalbert@hotmail.com>
2016-11-06 17:43:49 -0500

    added pub crawl pics

--- regisseur.js
+++ regisseur.js
@@ -7,7 +7,7 @@ ng_reg.controller('CarouselCtrl', function($scope){
     };
     $scope.init = function(location, caption_dict, pic_number){
         $scope.photos = [];
-        for (var i = 1; i<pic_number; i++){
+        for (var i = 1; i<=pic_number; i++){
             var caption = '';
             $scope.photos.push({src: location + i+'.jpg', caption: caption});
         }
